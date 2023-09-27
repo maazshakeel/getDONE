ea122d20c785058c5b418114dfb588306f07de7b
M. Maaz <maaz.shakeel.bodla@gmail.com>
2023-09-27 08:28:12 +0700

    feat: sample landing page example

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,60 @@
 "use client";
 
-import { Text } from "@chakra-ui/react";
+import {
+  Flex,
+  Container,
+  Heading,
+  Stack,
+  Text,
+  Button,
+} from "@chakra-ui/react";
+import Illustration from "./components/Illustration";
 
-export default function Home() {
+export default function CallToActionWithIllustration() {
   return (
-    <main>
-      <Text>Hello, Chakra!</Text>
-    </main>
+    <Container maxW={"5xl"}>
+      <Stack
+        textAlign={"center"}
+        align={"center"}
+        spacing={{ base: 8, md: 10 }}
+        py={{ base: 20, md: 28 }}
+      >
+        <Heading
+          fontWeight={600}
+          fontSize={{ base: "3xl", sm: "4xl", md: "6xl" }}
+          lineHeight={"110%"}
+        >
+          Meeting scheduling{" "}
+          <Text as={"span"} color={"main"}>
+            made easy
+          </Text>
+        </Heading>
+        <Text color={"gray.500"} maxW={"3xl"}>
+          Never miss a meeting. Never be late for one too. Keep track of your
+          meetings and receive smart reminders in appropriate times. Read your
+          smart “Daily Agenda” every morning.
+        </Text>
+        <Stack spacing={6} direction={"row"}>
+          <Button
+            rounded={"full"}
+            px={6}
+            colorScheme={"orange"}
+            bg={"main"}
+            _hover={{ bg: "orange.500" }}
+          >
+            Get started
+          </Button>
+          <Button rounded={"full"} px={6}>
+            Learn more
+          </Button>
+        </Stack>
+        <Flex w={"full"}>
+          <Illustration
+            height={{ sm: "24rem", lg: "28rem" }}
+            mt={{ base: 12, sm: 16 }}
+          />
+        </Flex>
+      </Stack>
+    </Container>
   );
 }
