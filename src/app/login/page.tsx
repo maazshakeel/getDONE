e83547faf072cb64dff8bd9703369f88afe797aa
M. Maaz <maaz.shakeel.bodla@gmail.com>
2023-09-28 10:34:50 +0700

    feat(login & register): login and register auth and style (navbar)

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,13 +1,15 @@
 "use client";
 
+import WithSubnavigation from "@/components/Navbar";
 import { Center, Container, Flex, Text } from "@chakra-ui/react";
 import { SignIn } from "@clerk/nextjs";
 
 export default function Login() {
   return (
-    <Container maxW={"full"} maxH={"full"} centerContent>
+    <Container maxW={"full"} maxH={"full"}>
+      <WithSubnavigation />
       <Flex>
-        <Center w="100vw" height="100vh">
+        <Center height={"85vh"} width={"100vw"}>
           <SignIn />
         </Center>
       </Flex>
--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,13 +1,15 @@
 "use client";
 
+import WithSubnavigation from "@/components/Navbar";
 import { Center, Container, Flex, Text } from "@chakra-ui/react";
 import { SignUp } from "@clerk/nextjs";
 
 export default function Register() {
   return (
-    <Container maxW={"full"} maxH={"full"} centerContent>
+    <Container maxW={"full"} maxH={"full"}>
+      <WithSubnavigation />
       <Flex>
-        <Center w="100vw" height="100vh">
+        <Center w="100vw" height="85vh">
           <SignUp />
         </Center>
       </Flex>
