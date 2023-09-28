02b473ab3fa19320297d65c79eca4f0cec94f4f6
M. Maaz <maaz.shakeel.bodla@gmail.com>
2023-09-28 07:58:28 +0700

    feat(layout): use clerk in the app

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,15 @@
 import ChakraUiProvider from "@/provider/chakra-ui.provider";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <html lang="en">
-      <body>
-        <ChakraUiProvider>{children}</ChakraUiProvider>
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body>
+          <ChakraUiProvider>{children}</ChakraUiProvider>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 };
 
