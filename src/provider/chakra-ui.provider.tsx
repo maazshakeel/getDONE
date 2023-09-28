a6616b16c279b61315f0095ebd62ed5434137c2f
M. Maaz <maaz.shakeel.bodla@gmail.com>
2023-09-28 10:35:32 +0700

    feat(spinner): add loading animation

--- src/provider/chakra-ui.provider.tsx
+++ src/provider/chakra-ui.provider.tsx
@@ -1,13 +1,30 @@
 "use client";
 
 import { CacheProvider } from "@chakra-ui/next-js";
-import { ChakraProvider } from "@chakra-ui/react";
+import { Center, ChakraProvider, Flex, Spinner } from "@chakra-ui/react";
 import { theme } from "../theme";
+import { ClerkLoaded, ClerkLoading } from "@clerk/nextjs";
 
 const ChakraUiProvider = ({ children }: { children: React.ReactNode }) => {
   return (
     <CacheProvider>
-      <ChakraProvider theme={theme}>{children}</ChakraProvider>
+      <ChakraProvider theme={theme}>
+        <ClerkLoading>
+          <Flex>
+            <Center height={"100vh"} width={"100vw"}>
+              <Spinner
+                thickness="4px"
+                speed="0.65s"
+                emptyColor="gray.200"
+                color="main"
+                size="xl"
+                alignSelf={"center"}
+              />
+            </Center>
+          </Flex>
+        </ClerkLoading>
+        <ClerkLoaded>{children}</ClerkLoaded>
+      </ChakraProvider>
     </CacheProvider>
   );
 };
